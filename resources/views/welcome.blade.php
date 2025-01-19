d2ce1e6a777715b431c5214a719164e6c3e0c153
prox108 <vicentejdelrio@gmail.com>
2025-01-19 19:59:37 -0300

    Capitulo 7 tailwind y vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
+
 </head>
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to the Ninja Network</h1>
     <p>Click the button below to view the list of ninjas.</p>
 
