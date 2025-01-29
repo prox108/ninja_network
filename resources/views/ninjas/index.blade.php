0c37ce53f53aec3b5b9115ff2d2a64f80d78d25e
prox108 <vicentejdelrio@gmail.com>
2025-01-28 22:48:05 -0300

    Capitulo 15, paginacion

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,13 +1,15 @@
 <x-layout>
-<h2>Currently Avaible Ninjas</h2>
+    <h2>Currently Avaible Ninjas</h2>
 
-<ul>
-    @foreach($ninjas as $ninja)
-    <li>
-        <x-card href="{{ route('ninjas.show', $ninja->id) }}" :highlight="$ninja['skill'] > 70">
-            <h3>{{ $ninja->name }}</h3>
-        </x-card>
-    </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach($ninjas as $ninja)
+        <li>
+            <x-card href="{{ route('ninjas.show', $ninja->id) }}" :highlight="$ninja['skill'] > 70">
+                <h3>{{ $ninja->name }}</h3>
+            </x-card>
+        </li>
+        @endforeach
+    </ul>
+
+    {{ $ninjas->links() }}
 </x-layout>
